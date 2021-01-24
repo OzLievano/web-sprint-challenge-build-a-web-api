6a1a918497bfb97a6714f5f50aa6c08e4004ee8f
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-01-24 14:37:44 -0500

    server and index js set up to listen for PORT 6000

--- api/server.js
+++ api/server.js
@@ -3,5 +3,15 @@ const server = express();
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+const actions = require('./actions/actions-router')
+const projects = require('./projects/projects-router')
+
+server.use(express.json())
+server.use('/api/actions',actions);
+server.use('/api/projects',projects)
+
+server.get('/',(req,res)=>{
+    res.send('<h1>Welcome to the Thunderdome</h1>');
+})
 
 module.exports = server;
--- index.js
+++ index.js
@@ -13,4 +13,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 Go code!
 */
 
-const name = "ozzy"
+const server = require('./api/server');
+
+const port = process.env.PORT;
+
+server.listen(port,()=>{
+    console.log(`Server Listening on port:${port}`)
+})
