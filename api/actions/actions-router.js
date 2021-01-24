f5ccb8c29eaafe8a1c0367d625f010c2f3f42e92
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-01-24 14:44:25 -0500

    get requests for actions completed

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,4 +4,33 @@ const Actions = require('../actions/actions-model')
 
 const router = express.Router();
 
+const fiveHundredMessage = "System error, please view get request."
+// get array of actions (or an empty array) as the body of the response
 
+router.get('/',(req,res)=>{
+    Actions.get()
+    .then((actions)=>{
+        res.status(200).json(actions)
+    })
+    .catch((error)=>{
+        res.status(500).json({error:fiveHundredMessage})
+    })
+})
+
+// get array of actions for a given id as the body of the resposne
+
+router.get('/:id',(req,res)=>{
+    const {id}= req.params;
+    // if ID exists return actions
+    if(id){
+        Actions.get(id)
+        .then((actions)=>{
+            res.status(200).json(actions)
+        })
+        .catch((error)=>{
+            res.status(500).json({error:fiveHundredMessage})
+        })
+    }else{
+        res.status(404).json({error:"ID does not exist."})
+    }
+})
